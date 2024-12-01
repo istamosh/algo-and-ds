5c429532c994723725f34f2de2115901adedb26a
istamosh <63596969+istamosh@users.noreply.github.com>
2024-12-02 02:07:13 +0700

    update: more db queries

--- queries.sql
+++ queries.sql
@@ -12,3 +12,12 @@ SELECT * FROM CITY WHERE ID = 1661;
 
 -- Display all the fields in CITY table in the JPN country.
 SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN';
+
+-- (DB2) Display the names (using c alias of CITY table) of all the cities in the JPN country.
+SELECT c.NAME FROM CITY c WHERE COUNTRYCODE = 'JPN';
+
+-- Display the names of all the cities and state columns in the STATION table.
+SELECT CITY, STATE FROM STATION;
+
+-- Display distinct city names from the STATION table with even ID values and order city in ascending order.
+SELECT DISTINCT CITY FROM STATION WHERE MOD(ID, 2) = 0 ORDER BY CITY ASC;
